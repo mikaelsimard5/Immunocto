80170df2c8408f2abaa468966b19daa3cdee6cf9
Mikael Simard <mikael.simard5@gmail.com>
2024-12-09 10:19:59 +0000

    Latest configuration files

--- ConvNet/Infer_Cell_Classifier.py
+++ ConvNet/Infer_Cell_Classifier.py
@@ -1,7 +1,11 @@
 import lightning as L
+import numpy as np
+import pandas as pd
 from sklearn import preprocessing
+import os
 import toml
 import torch
+from torch.utils.data import DataLoader
 
 from Data_loading.dataloader_digpat import get_tile_dataset, DataGenerator, digpat_datamodule
 from models.HE_mask_classifiers import SAM_ConvNet
@@ -44,7 +48,7 @@ def main(config_file=None, n_gpus=1, gpu_devices=0):
                         accelerator="gpu",
                         strategy="ddp",
                         logger=False,
-                        precision=trainer_config['BASEMODEL']['Precision'],
+                        precision=trainer_config['BASEMODEL']['precision'],
                         use_distributed_sampler = False,
                         benchmark=False)        
 
@@ -63,7 +67,7 @@ def main(config_file=None, n_gpus=1, gpu_devices=0):
     _, val_transform = get_transforms(trainer_config)
 
     data = DataLoader(DataGenerator(tile_dataset, config=config, transform=val_transform),
-                      batch_size = config['BASEMODEL']['Batch_Size_Classif'],
+                      batch_size = config['BASEMODEL']['batch_size'],
                       num_workers = num_workers,
                       pin_memory = False,
                       shuffle = False) 
@@ -106,7 +110,8 @@ def main(config_file=None, n_gpus=1, gpu_devices=0):
     # Merge into a single dataframe on rank 0.
     if trainer.is_global_zero:
         
-        export_filename = './inference_results/'
+        export_filename = config['EXPORT'].get('inference_results', './inference_results/results.csv')
+        os.makedirs(os.path.dirname(export_filename), exist_ok=True)
 
         tile_dataset_dict = {}
         for gpu_id in range(trainer.world_size):
@@ -120,100 +125,11 @@ def main(config_file=None, n_gpus=1, gpu_devices=0):
 
 
         full_df = pd.concat([value for key, value in tile_dataset_dict.items()], axis=0)
-
-        #[MS] TEMPORARY ADJUSTED FOR PREDICTIONS-------
-        
-        #"""
-        print(full_df)
-
-        potatoes
-        """
-        full_df['predicted_class'] = full_df[["cd4","cd8","bcell","macrophage","other"]].idxmax(axis=1)
-        
-        lcm = {'other':'other', 'cd4':'lymphocyte', 'cd8':'lymphocyte', 'bcell':'lymphocyte', 'macrophage':'other'}
-        full_df['bc'] = full_df['class'].map(lcm)
-        full_df['predicted_bc'] = full_df['predicted_class'].map(lcm)
-
-        print(full_df['bc'].value_counts())
-        print(full_df['predicted_bc'].value_counts())
-        # Loop through the dataframe to count TP, FN, FP, TN
-        TP = FN = FP = TN = 0
-        for i, row in full_df.iterrows():
-            if row['bc'] == 'lymphocyte' and row['predicted_bc'] == 'lymphocyte':
-                TP += 1
-            elif row['bc'] == 'lymphocyte' and row['predicted_bc'] == 'other':
-                FN += 1
-            elif row['bc'] == 'other' and row['predicted_bc'] == 'lymphocyte':
-                FP += 1
-            elif row['bc'] == 'other' and row['predicted_bc'] == 'other':
-                TN += 1
-
-        # Print the counts
-        print(f'True Positives (TP): {TP}')
-        print(f'False Negatives (FN): {FN}')
-        print(f'False Positives (FP): {FP}')
-        print(f'True Negatives (TN): {TN}')
-        #"""
-        #[MS] TEMPORARY ADJUSTED FOR PREDICTIONS-------
-        
         full_df.to_csv(export_filename, index=False)
-
-
-
+        print(f"Inference results exported to :{export_filename}.")
 
     trainer.strategy.barrier() # synchronise before looping to next patient if there
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """
-    model_name = generate_model_name(config)
-    logger = get_logger(config, model_name)
-    callbacks = get_callbacks(config, model_name=model_name)
-    L.seed_everything(42, workers=True)
-    torch.set_float32_matmul_precision('medium')
-
-    _, val_transform = get_transforms(config) # but with the config model.
-    label_encoder = preprocessing.LabelEncoder()
-    label_encoder.fit(tile_dataset['label']) 
-
-    # Encode tile_dataset classes
-    tile_dataset['label'] = label_encoder.transform(tile_dataset['label'])
-
-    trainer = L.Trainer(devices=gpu_devices,
-                        strategy="ddp",
-                        accelerator="gpu",
-                        max_epochs=config['ADVANCEDMODEL']['max_epochs'],
-                        precision=config['BASEMODEL']['precision'],
-                        callbacks=callbacks,
-                        logger=logger,
-                        check_val_every_n_epoch=1,
-                        log_every_n_steps=5,
-                        sync_batchnorm=True)
-
-    config['DATA']['weights'] = get_class_weights(tile_dataset, label_encoder)   
-    data = digpat_datamodule(tile_dataset, config=config, train_transform=train_transform, val_transform=val_transform)
-
-    print("********************************************************************************************************")
-    # Train model
-
-    model = SAM_ConvNet(config, LabelEncoder = label_encoder)
-    trainer.fit(model, datamodule=data)
-
-    if config['DATA']['test_size']>0:    
-        trainer.test(model, data.test_dataloader())
-
-    """
 
 
 if __name__ == "__main__":
